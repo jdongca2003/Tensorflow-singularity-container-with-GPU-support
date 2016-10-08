559a7638f3e59f1ef511b33ce7e6438a9242dac0
jdongca2003 <jdongca2003@gmail.com>
2016-10-08 22:24:41 +0000

    remove irrelevant line

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ singularity create ubuntu_tensorflow_GPU.img
 singularity expand --size 5000 ubuntu_tensorflow_GPU.img
 singularity bootstrap ubuntu_tensorflow_GPU.img ubuntu.def
 singularity exec -w ubuntu_tensorflow_GPU.img sh ./tensorflow.sh
-singularity exec -w ubuntu_tensorflow_GPU.img cp python_tensorflow /usr/bin/
