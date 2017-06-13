f86e2512d70af015940c11c39ea19c291b8eeb3b
jdongca2003 <jdongca2003@gmail.com>
2017-06-13 17:26:49 +0000

    add nv flag

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ sudo rm -f $IMAGE
 sudo singularity create $IMAGE
 sudo singularity expand --size 5000 $IMAGE
 sudo singularity bootstrap $IMAGE $DEF
-sudo singularity exec -B `pwd`:/mnt -w $IMAGE sh /mnt/tensorflow.sh
+sudo singularity exec --nv -B `pwd`:/mnt -w $IMAGE sh /mnt/tensorflow.sh
